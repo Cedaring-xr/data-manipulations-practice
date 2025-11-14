628f0077959720cf558a9ec3cdbf972fb16c5431
mattraydev <cedaring.xr@gmail.com>
2025-11-13 20:20:50 -0700

    added some more functions

--- completed/basic/basic.js
+++ completed/basic/basic.js
@@ -55,9 +55,69 @@ const capital2 = (str) => {
 }
 console.log(capital2('another type of string'))
 // 8. Given an array, count the number of duplicate values
+function countDuplicates(arr) {
+	const counts = {}
+	let duplicates = 0
 
+	arr.forEach((num) => {
+		counts[num] = (counts[num] || 0) + 1
+	})
+
+	for (const key in counts) {
+		if (counts[key] > 1) {
+			duplicates += counts[key] - 1
+		}
+	}
+
+	return duplicates
+} // O(n)
+console.log(countDuplicates([1, 3, 4, 5, 6, 4, 3, 6, 4, 5, 3, 5]))
+
+function dupes(arr) {
+	let count = 0
+	let sorted = [...arr].sort((a, b) => a - b)
+	for (let i = 1; i < sorted.length; i++) {
+		if (sorted[i] === sorted[i - 1]) {
+			count++
+		}
+	}
+	return count
+} // O(n log n)
+console.log(dupes([1, 2, 3, 4, 5, 6, 4, 5, 6, 4, 54, 5, 2]))
 // 9. Given a string of text, find the longest word in the string and return the length and the word
+const longestWord = (str) => {
+	let longest = 0
+	const newArr = str.split(' ')
+	for (let word of newArr) {
+		if (word.length > longest) {
+			longest = word.length
+		}
+	}
+	return longest
+}
+console.log(longestWord('test this string for longest word'))
 
-// 10. Given two different words as strings, check if the two are anagrams of each other
-
-// 11.
+// 10. Write a function that sorts letters of a string alphabetically
+function alphaSort(str) {
+	return str.split('').sort().join()
+}
+console.log(alphaSort('some string of letters'))
+// 11. Given two different words as strings, check if the two are anagrams of each other
+const anagrams = (str1, str2) => {
+	let letters1 = str1.split('').sort()
+	let letters2 = str2.split('').sort()
+	if (letters1.length !== letters2.length) return false
+	return letters1.every((val, i) => val === letters2[i])
+}
+console.log(anagrams('test', 'worda'))
+console.log(anagrams('test', 'sett'))
+// 12. Write a function that takes in an unknown number of string arguments and returns them sorted alphabetically
+function sortAll(...args) {
+	const returnList = []
+	for (word of args) {
+		returnList.push(word.split('').sort().join(''))
+	}
+	return returnList
+}
+console.log(sortAll('test', 'other', 'third', 'more'))
+console.log(sortAll('something', 'another'))
