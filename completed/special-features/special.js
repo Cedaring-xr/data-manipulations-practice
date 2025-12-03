565358c79dc72a7e0f6cbc016f9045d46a864286
mattraydev <cedaring.xr@gmail.com>
2025-12-02 20:53:05 -0700

    special questions

--- completed/special-features/special.js
+++ completed/special-features/special.js
@@ -1,3 +1,5 @@
 // 1. Write a function that takes in a Date object and returns a formatted string
 
 // 2. Write a function that validates a email address format using regex
+
+// 3. Write a recursive function that can be used for updating user attributes
